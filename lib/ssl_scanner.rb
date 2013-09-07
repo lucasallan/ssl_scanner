95b03ea6ab64cc8618f99e21d1db9537782d443e
Lucas Allan Amorim <lucas.allan@gmail.com>
2013-09-07 20:35:29 -0300

    code improvements

--- lib/ssl_scanner.rb
+++ lib/ssl_scanner.rb
@@ -4,33 +4,27 @@ require 'net/https'
 module SslScanner
 
   def self.scan_domain(domain)
-    perform_request(domain)
+    cert = scan(domain)
 
-    raise 'no ssl information found' if @cert.nil?
+    return { error: 'no ssl information found' } if cert.nil?
 
-    @info = {
-      subject:      @cert.subject,
-      issuer:       @cert.issuer,
-      serial:       @cert.serial,
-      issued:       @cert.not_before,
-      expires:      @cert.not_after,
-      certificate:  @cert.to_s
+    {
+      subject:      cert.subject,
+      issuer:       cert.issuer,
+      serial:       cert.serial,
+      issued:       cert.not_before,
+      expires:      cert.not_after,
+      certificate:  cert.to_s
     }
-
-    @info
-  rescue => ex
-    { error: ex.to_s }
   end
 
   private
 
-  def self.perform_request(domain)
-    uri = URI.parse(domain)
-    http = Net::HTTP.new(uri.host,uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    http.start do |h|
-      @cert = h.peer_cert
+    def self.scan(domain)
+      uri = URI.parse(domain)
+      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |h|
+        return h.peer_cert
+      end
+    rescue
     end
-  end
 end
